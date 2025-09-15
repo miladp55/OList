bec116a83a08f3a7889d573f0d60c11b766d167b
MILAD PARVIZI <miladp55@gmail.com>
2025-09-15 18:51:27 +0330

    add license

--- OptimizedList/OptimizedList/OptimizedList/OList.cs
+++ OptimizedList/OptimizedList/OptimizedList/OList.cs
@@ -4,6 +4,7 @@ using System.Collections;
 namespace OptimizedGenericList
 {
     /// <summary>
+    /// Developed by Milad Parvizi, GitHub:miladp55
     /// A high-performance, memory-optimized list to reduce GC pressure.
     /// This class is intended for scenarios where performance and memory allocation are critical.
     /// It is not thread-safe by default yet.
