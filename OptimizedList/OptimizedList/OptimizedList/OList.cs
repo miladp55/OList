cf9bc68ebfa3d40cd01e3fce563ed52d314b7861
MILAD PARVIZI <miladp55@gmail.com>
2025-09-15 19:53:16 +0330

    add asynchronous and add new methods

--- OptimizedList/OptimizedList/OptimizedList/OList.cs
+++ OptimizedList/OptimizedList/OptimizedList/OList.cs
@@ -1,5 +1,6 @@
 ﻿using System.Buffers;
 using System.Collections;
+using System.Runtime.CompilerServices;
 
 namespace OptimizedGenericList
 {
@@ -10,12 +11,11 @@ namespace OptimizedGenericList
     /// It is not thread-safe by default yet.
     /// </summary>
     /// <typeparam name="T">The type of element in the list.</typeparam>
-    public sealed class OList<T> : IEnumerable<T>, IDisposable
+    public sealed class OList<T> : IEnumerable<T>, IAsyncEnumerable<T>, IDisposable
     {
         private T[] _items;
         private int _size;
         private const int DefaultCapacity = 4;
-
         /// <summary>
         /// Gets the number of elements contained in the OptimizedList.
         /// </summary>
@@ -24,7 +24,8 @@ namespace OptimizedGenericList
         /// Gets the total number of elements the internal data structure can hold without resizing.
         /// </summary>
         public int Capacity => _items.Length;
-
+        
+        
         /// <summary>
         /// Initializes a new instance of the OptimizedList<T> class that is empty and has the default initial capacity.
         /// </summary>
@@ -33,7 +34,6 @@ namespace OptimizedGenericList
             _items = ArrayPool<T>.Shared.Rent(DefaultCapacity);
             _size = 0;
         }
-
         /// <summary>
         /// Initializes a new instance of the OptimizedList<T> class that is empty and has the specified initial capacity.
         /// </summary>
@@ -45,8 +45,6 @@ namespace OptimizedGenericList
             _items = ArrayPool<T>.Shared.Rent(capacity);
             _size = 0;
         }
-
-
         /// <summary>
         /// Adds an object to the end of the OptimizedList<T>.
         /// This operation is O(1) on average, but can be O(n) if a resize is required.
@@ -60,8 +58,52 @@ namespace OptimizedGenericList
             }
             _items[_size++] = item;
         }
-
-
+        /// <summary>
+        /// Asynchronously adds an object to the end of the OptimizedList<T>.
+        /// In this implementation, the async version doesn't provide concurrency benefits
+        /// as the underlying collection is not thread-safe. It's provided for API consistency.
+        /// </summary>
+        /// <param name="item">The object to be added.</param>
+        /// <returns>A completed task.</returns>
+        public Task AddAsync(T item)
+        {
+            Add(item);
+            return Task.CompletedTask;
+        }
+        /// <summary>
+        /// Adds the elements of the specified collection to the end of the OptimizedList<T>.
+        /// Optimized using Span<T> for performance.
+        /// </summary>
+        /// <param name="collection">The collection whose elements should be added.</param>
+        public void AddRange(IEnumerable<T> collection)
+        {
+            if (collection is ICollection<T> c)
+            {
+                int count = c.Count;
+                if (count > 0)
+                {
+                    EnsureCapacity(_size + count);
+                    c.CopyTo(_items, _size);
+                    _size += count;
+                }
+            }
+            else
+            {
+                foreach (T item in collection)
+                {
+                    Add(item);
+                }
+            }
+        }
+        /// <summary>
+        /// Asynchronously adds the elements of the specified collection to the end of the OptimizedList<T>.
+        /// </summary>
+        /// <param name="collection">The collection whose elements should be added.</param>
+        public Task AddRangeAsync(IEnumerable<T> collection)
+        {
+            AddRange(collection);
+            return Task.CompletedTask;
+        }
         /// <summary>
         /// Ensures that the capacity of this list is at least the given minimum value.
         /// If the current capacity is less than min, the capacity is increased to twice the
@@ -81,7 +123,6 @@ namespace OptimizedGenericList
 
             _items = newItems;
         }
-
         public void Dispose()
         {
             if (_items is not null)
@@ -91,7 +132,6 @@ namespace OptimizedGenericList
                 _size = 0;
             }
         }
-
         public IEnumerator<T> GetEnumerator()
         {
             for (int i = 0; i < _size; i++)
@@ -99,10 +139,24 @@ namespace OptimizedGenericList
                 yield return _items[i];
             }
         }
-
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
+        }
+        public async IAsyncEnumerator<T> GetAsyncEnumerator([EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            for (int i = 0; i < _size; i++)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+                await Task.Yield();
+                yield return _items[i];
+            }
+        }
+
+        IAsyncEnumerator<T> IAsyncEnumerable<T>.GetAsyncEnumerator(CancellationToken cancellationToken)
+        {
+            // Call the public GetAsyncEnumerator and return the resulting iterator object.
+            return GetAsyncEnumerator(cancellationToken);
         }
     }
 }
--- OptimizedList/OptimizedList/OptimizedList/Program.cs
+++ OptimizedList/OptimizedList/OptimizedList/Program.cs
@@ -8,7 +8,28 @@ ols.Add(14);
 ols.Add(16);
 ols.Add(18);
 
+
+ols.AddRange(new[] { 30, 40, 50 });
+
 Console.WriteLine($"Count: {ols.Count}");
 Console.WriteLine($"Capacity: {ols.Capacity}");
 
 foreach( int i in ols ) Console.WriteLine($"Final List: item value: {i}");
+
+
+
+// Synchronous iteration
+Console.WriteLine("Sync foreach:");
+foreach (var item in ols)
+{
+    Console.WriteLine(item);
+}
+
+// Asynchronous iteration (a feature not in the standard List<T>)
+Console.WriteLine("\nAsync foreach:");
+await foreach (var item in ols)
+{
+    Console.WriteLine(item);
+}
+
+Console.ReadKey();
